f232caa5b820fde8d69d1876d10f381e7e5c193e
Felipe Rabelo <carbonemasseo@hotmail.com>
2018-01-24 14:27:27 -0200

    Last commit of the day

--- workspace/binaryTree/tree.go
+++ workspace/binaryTree/tree.go
@@ -18,20 +18,32 @@ func insert (tree *node, i int) {
 	if (i < tree.value && tree.left == nil) || (i >= tree.value && tree.right == nil) {
 		if i < tree.value {
 			tree.left = &node {nil, nil, i}
-		} else {tree.right = &node {nil, nil, i}}
+		} else {
+			tree.right = &node {nil, nil, i}
+		}
 	} else {
 		if i < tree.value {
 			insert(tree.left, i)
-		} else {insert(tree.right, i)}
+		} else {
+			insert(tree.right, i)
+		}
 	}
 }
 
 func main () {
-	tree := node {nil, nil, 4}
+	var x = []int {
+		48,96,86,68,
+		57,82,63,70,
+		37,34,83,27,
+		19,97, 9,17,
+	}
 
-	insert(&tree, 3)
-	insert(&tree, 5)
-	insert(&tree, 2)
+	arrHead := x[0:1][0]
+	tree := node {nil, nil, arrHead}
+
+	for _, i := range x[1:] {
+		insert(&tree, i)
+	}
 
 	printTree(&tree)
 }
