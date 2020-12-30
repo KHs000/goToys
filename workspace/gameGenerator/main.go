38a92cb954487ca60d924e861883e0560b6c1e63
Felipe Rabelo <felipe.masseo@bancointer.com.br>
2020-12-30 11:23:31 -0300

    optimizations

--- workspace/gameGenerator/main.go
+++ workspace/gameGenerator/main.go
@@ -18,24 +18,24 @@ func initializePool() []int {
 }
 
 func remove(s []int, i int) []int {
-	s[len(s)-1], s[i] = s[i], s[len(s)-1]
+	s[i] = s[len(s)-1]
 	return s[:len(s)-1]
 }
 
 func drawNumbers(pool []int) (numbers []int, newPool []int) {
 	var game []int
 
-	seed := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(seed)
+	if len(pool) == 6 {
+		game = pool
+	} else {
+		seed := rand.NewSource(time.Now().UnixNano())
+		r := rand.New(seed)
 
-	for i := 0; i < 6; i++ {
-		idx := 0
-		if len(pool) > 1 {
-			idx = r.Intn(len(pool) - 1)
+		for i := 0; i < 6; i++ {
+			idx := r.Intn(len(pool) - 1)
+			game = append(game, pool[idx])
+			pool = remove(pool, idx)
 		}
-
-		game = append(game, pool[idx])
-		pool = remove(pool, idx)
 	}
 
 	sort.Ints(game)
